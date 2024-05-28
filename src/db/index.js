85de8f62eaaead16d0432d6946858f0019efbaf4
Albert Yanit <ayanit1@googlemail.com>
2024-05-28 08:37:47 +0100

    Add setting up DB

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const databaseDomain = '127.0.0.1:27017';
+const databaseName = 'test';
+
+const mongoDBUri = `mongodb://${databaseDomain}/${databaseName}`;
+
+const connectDB = () => {
+  const db = mongoose.connection;
+
+  // Bind connection to error event (to get notification of connection errors)
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+  // Bind connection to open event (to get notification when the connection is successful)
+  db.once('open', () => console.log('Successfully connected to MongoDB!'));
+
+  mongoose.connect(mongoDBUri);
+};
+
+module.exports = connectDB;
